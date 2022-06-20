c417aabbe65d2a6778f58bfff64c51f1665b67c9
ST0PHoon <codingnb306@naver.com>
2022-06-20 16:25:40 +0900

    selectOne, selectAll Test 구현

--- src/main/test/kr/ac/kopo/ctc/kopo37/dao/ScoreItemDaoTest.java
+++ src/main/test/kr/ac/kopo/ctc/kopo37/dao/ScoreItemDaoTest.java
@@ -10,25 +10,25 @@ class ScoreItemDaoTest {
 
 	private ScoreItemDao scoreItemDao = new ScoreItemDaoImpl();
 	
-	@Test
-	void testinsert() {
-		ScoreItem scoreItemInput = new ScoreItem();
-		scoreItemInput.setName("나연");
-		scoreItemInput.setStudentId(209920);
-		scoreItemInput.setKor(95);
-		scoreItemInput.setEng(100);
-		scoreItemInput.setMat(95);
-		
-		scoreItemDao.insert(scoreItemInput);
-		
-		ScoreItem scoreItemOutput = scoreItemDao.selectOne(209920);
-		
-		assertEquals(scoreItemOutput.getName(), scoreItemInput.getName());
-		assertEquals(scoreItemOutput.getId(), scoreItemInput.getId());
-	}
+//	@Test
+//	void testinsert() {
+//		ScoreItem scoreItemInput = new ScoreItem();
+//		scoreItemInput.setName("나연");
+//		scoreItemInput.setStudentId(209920);
+//		scoreItemInput.setKor(95);
+//		scoreItemInput.setEng(100);
+//		scoreItemInput.setMat(95);
+//		
+//		scoreItemDao.insert(scoreItemInput);
+//		
+//		ScoreItem scoreItemOutput = scoreItemDao.selectOne(209920);
+//		
+//		assertEquals(scoreItemOutput.getName(), scoreItemInput.getName());
+//		assertEquals(scoreItemOutput.getId(), scoreItemInput.getId());
+//	}
 	
 //	@Test
-//	void testdelete() {
+//	void testDelete() {
 //
 //		int id = 209920;
 //		
@@ -61,6 +61,25 @@ class ScoreItemDaoTest {
 //		assertEquals(40, scoreItemOutput.getMat());
 //	}
 	
+	@Test
+	void testSelectOne() {
+		ScoreItem scoreItemOutput = new ScoreItem();
+		
+		scoreItemOutput = scoreItemDao.selectOne(209901);
+		
+		assertEquals("나연", scoreItemOutput.getName());
+		assertEquals(209901, scoreItemOutput.getStudentId());
+		assertEquals(40, scoreItemOutput.getKor());
+		assertEquals(40, scoreItemOutput.getEng());
+		assertEquals(40, scoreItemOutput.getMat());
+	}
 	
+	@Test
+	void testSelectAll() {
+		
+		int afterDataLength = scoreItemDao.selectTotalCount();
+		
+		assertEquals(19, afterDataLength);
+	}
 
 }
