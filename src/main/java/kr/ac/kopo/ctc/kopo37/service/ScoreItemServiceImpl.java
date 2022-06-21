174aee4b3e565581b33d8ec58370c9653271ac7f
ST0PHoon <codingnb306@naver.com>
2022-06-21 16:13:43 +0900

    serviceimpl 기능 구현

--- src/main/java/kr/ac/kopo/ctc/kopo37/service/ScoreItemServiceImpl.java
+++ src/main/java/kr/ac/kopo/ctc/kopo37/service/ScoreItemServiceImpl.java
@@ -6,38 +6,76 @@ import kr.ac.kopo.ctc.kopo37.dao.ScoreItemDao;
 import kr.ac.kopo.ctc.kopo37.dao.ScoreItemDaoImpl;
 import kr.ac.kopo.ctc.kopo37.domain.ScoreItem;
 import kr.ac.kopo.ctc.kopo37.dto.Pagination;
+import kr.ac.kopo.ctc.kopo37.dto.ScoreItemsDto;
 
 public class ScoreItemServiceImpl implements ScoreItemService {
 	private ScoreItemDao scoreItemDao = new ScoreItemDaoImpl();
+	private final int countPerPage = 10;
+	private final int pageSize = 10;
 
 	@Override
-	public ScoreItemDao getScoreItemDao() {
-		return scoreItemDao;
+	public ScoreItemsDto scoreItemSelectAll(String strCPage) {
+		// 현재 페이지 번호 null 체크
+		int cPage = 0;
+		if (strCPage == null) {
+			cPage = 1;
+		} else {
+			cPage = Integer.parseInt(strCPage);
+		}
+		
+		// 레코드 목록 조회
+		ScoreItemsDto scoreItemsDto = new ScoreItemsDto();
+		int startRecordNo = (cPage - 1) * countPerPage;
+		
+		// 페이징 처리
+		List<ScoreItem> ScoreItems = scoreItemDao.selectAll(startRecordNo, countPerPage);
+		int totalRecordCount = scoreItemDao.selectTotalCount();	// 전체 레코드 수 조회
+		Pagination pagination = getPagination(cPage, countPerPage, pageSize, totalRecordCount);
+		
+		scoreItemsDto.setScoreItems(ScoreItems);
+		scoreItemsDto.setPagination(pagination);
+
+		return scoreItemsDto;
 	}
 
 	@Override
-	public void setScoreItemDao(ScoreItemDao scoreItemDao) {
-		this.scoreItemDao = scoreItemDao;
+	public List<ScoreItem> selectScoreItemsByName(String name) {
+		return scoreItemDao.selectName(name);
 	}
 
 	@Override
-	public ScoreItem create(ScoreItem scoreItem) {
-		// TODO Auto-generated method stub
-		return null;
+	public ScoreItem scoreItemSelectById(int id) {
+		return  scoreItemDao.selectOne(id);
 	}
 
 	@Override
-	public ScoreItem selectOne(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public void scoreItemInsert(String name, int kor, int eng, int mat) {
+		int newId = scoreItemDao.selectNewId();
+		int firstId = scoreItemDao.selectFirstId();
+		
+		if (firstId != 209901) {
+			newId = 209901;
+		}
+		
+		ScoreItem scoreItem = new ScoreItem();
+		
+		scoreItem.setId(newId);
+		scoreItem.setName(name);
+		scoreItem.setKor(kor);
+		scoreItem.setEng(eng);
+		scoreItem.setMat(mat);
 	}
 
 	@Override
-	public List<ScoreItem> selectAll(int page, int countPerPage) {
-		// TODO Auto-generated method stub
-		return null;
+	public void scoreItemUpdate(ScoreItem scoreItem) {
+		scoreItemDao.update(scoreItem);
 	}
 
+	@Override
+	public void scoreItemDelete(int id) {
+		scoreItemDao.delete(id);
+	}
+	
 	@Override
 	public Pagination getPagination(int currentPage, int countPerPage, int pageSize, int totalCount) {
 		// currPage : 현재 페이지 , countPerPage : 한 페이지에 나올 숫자 목록, pageSize : 한 페이지 목록 수,
@@ -91,4 +129,5 @@ public class ScoreItemServiceImpl implements ScoreItemService {
 
 		return pagination;
 	}
+
 }
