b49dd96345220ac66a37ac6e9917a6fdd6d58c91
ST0PHoon <codingnb306@naver.com>
2022-06-25 13:18:54 +0900

    필요한 페이지 변수 추가

--- src/main/java/kr/ac/kopo/ctc/kopo37/dto/Pagination.java
+++ src/main/java/kr/ac/kopo/ctc/kopo37/dto/Pagination.java
@@ -1,11 +1,20 @@
 package kr.ac.kopo.ctc.kopo37.dto;
 
 public class Pagination {
-	private int ppPage;
-	private int pPage;
-	private int nPage;
-	private int nnPage;
-	private int cPage;
+	private int ppPage;	//<<: 첫 페이지로
+	private int pPage;	//< : 이전 숫자 그룹의 첫 페이지로
+	
+	private int nPage;	//> : 다음 숫자 그룹의 첫 숫자
+	private int nnPage;	//>> : 마지막 페이지로
+	
+	private int cPage;	// 현재 페이지
+	private int startPage;	// 현재 그룹의 시작 페이지 번호
+	private int lastPage;	// 현재 그룹의 마지막 페이지 번호
+	
+	private int countPerpage;	// 한 페이지에 나올 숫자 목록
+	private int pageSize;	//한 페이지 목록 수
+	private int totalCount;	//전체 목록 수
+	private int totalPage;	//전체 페이지 수
 	
 	public int getPpPage() {
 		return ppPage;
@@ -37,6 +46,40 @@ public class Pagination {
 	public void setcPage(int cPage) {
 		this.cPage = cPage;
 	}
-	
-	
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getLastPage() {
+		return lastPage;
+	}
+	public void setLastPage(int lastPage) {
+		this.lastPage = lastPage;
+	}
+	public int getCountPerpage() {
+		return countPerpage;
+	}
+	public void setCountPerpage(int countPerpage) {
+		this.countPerpage = countPerpage;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public int getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+	public int getTotalPage() {
+		return totalPage;
+	}
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
 }
